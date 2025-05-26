46370f83cd3c0dd8484a4b2e700149de1296253c
Diego Navarrete <dinavarrete@ug.uchile.cl>
2025-05-26 11:58:24 -0400

    Feat: created basic views for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get "jugadors/index"
-  get "jugadors/girar"
-  get "jugadors/reiniciar"
+  resources :jugadors, only: [:index] do
+    collection do
+      get :girar
+      get :reiniciar
+    end
+  end
+  root 'jugadors#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
