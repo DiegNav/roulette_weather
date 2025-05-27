e052de4a56f3a860e74e6b1e1e85399ad6334216
Diego Navarrete <dinavarrete@ug.uchile.cl>
2025-05-26 20:30:43 -0400

    Feat: Enhance Ruleta UI with improved button accessibility and styling; add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './app/views/**/*.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/javascripts/**/*.js',
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
